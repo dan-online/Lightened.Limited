64c585a0b21b2f6c4a10979f34f1ba6dffaed0b6
Dan <dan@dancodes.online>
2020-05-05 10:48:39 +0200

    placekitten

--- Projects.ts
+++ Projects.ts
@@ -4,7 +4,7 @@ export default {
       name: "Lorem ipsum number ",
       url: "https://dancodes.online",
       id: "test",
-      image: "https://cdn.dancodes.online/i/Monkey-War.gif?cache=1",
+      image: "http://placekitten.com/800/500?cache=1",
       desc:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris pretium, sodales lorem ac, malesuada augue. Vestibulum aliquet molestie ante, quis feugiat nibh congue id. Sed luctus turpis lacinia leo auctor, vitae aliquam turpis feugiat. Cras viverra finibus orci, ac porttitor ligula efficitur et."
     },
@@ -12,7 +12,7 @@ export default {
       name: "Lorem ipsum number 1",
       url: "https://dancodes.online",
       id: "test1",
-      image: "https://cdn.dancodes.online/i/Monkey-War.gif?cache=2",
+      image: "http://placekitten.com/800/500?cache=2",
       desc:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris pretium, sodales lorem ac, malesuada augue. Vestibulum aliquet molestie ante, quis feugiat nibh congue id. Sed luctus turpis lacinia leo auctor, vitae aliquam turpis feugiat. Cras viverra finibus orci, ac porttitor ligula efficitur et."
     },
@@ -20,7 +20,7 @@ export default {
       name: "Lorem ipsum number 1",
       url: "https://dancodes.online",
       id: "test2",
-      image: "https://cdn.dancodes.online/i/Monkey-War.gif?cache=3",
+      image: "http://placekitten.com/800/500?cache=3",
       desc:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris pretium, sodales lorem ac, malesuada augue. Vestibulum aliquet molestie ante, quis feugiat nibh congue id. Sed luctus turpis lacinia leo auctor, vitae aliquam turpis feugiat. Cras viverra finibus orci, ac porttitor ligula efficitur et."
     },
@@ -28,7 +28,7 @@ export default {
       name: "Lorem ipsum number 1",
       url: "https://dancodes.online",
       id: "test3",
-      image: "https://cdn.dancodes.online/i/Monkey-War.gif?cache=4",
+      image: "http://placekitten.com/800/500?cache=4",
       desc:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris pretium, sodales lorem ac, malesuada augue. Vestibulum aliquet molestie ante, quis feugiat nibh congue id. Sed luctus turpis lacinia leo auctor, vitae aliquam turpis feugiat. Cras viverra finibus orci, ac porttitor ligula efficitur et."
     },
@@ -36,7 +36,7 @@ export default {
       name: "Lorem ipsum number 1",
       url: "https://dancodes.online",
       id: "test4",
-      image: "https://cdn.dancodes.online/i/Monkey-War.gif?cache=5",
+      image: "http://placekitten.com/800/500?cache=5",
       desc:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris pretium, sodales lorem ac, malesuada augue. Vestibulum aliquet molestie ante, quis feugiat nibh congue id. Sed luctus turpis lacinia leo auctor, vitae aliquam turpis feugiat. Cras viverra finibus orci, ac porttitor ligula efficitur et."
     },
@@ -44,7 +44,7 @@ export default {
       name: "Lorem ipsum number 1",
       url: "https://dancodes.online",
       id: "test5",
-      image: "https://cdn.dancodes.online/i/Monkey-War.gif?cache=6",
+      image: "http://placekitten.com/800/500?cache=6",
       desc:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris pretium, sodales lorem ac, malesuada augue. Vestibulum aliquet molestie ante, quis feugiat nibh congue id. Sed luctus turpis lacinia leo auctor, vitae aliquam turpis feugiat. Cras viverra finibus orci, ac porttitor ligula efficitur et."
     }
