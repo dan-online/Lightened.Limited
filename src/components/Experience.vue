bb5fc5256e2939e0e8057cd5930fcd83ebdbce5e
DanCodes <dan@dancodes.online>
2020-05-10 16:05:29 +0200

    navbar and experiences in

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,38 +1,47 @@
 <template>
-  <b-container fluid>
+  <b-container>
     <b-row>
-      <b-col md="4"> </b-col>
-      <b-col md="8">
-        <b-row>
-          <b-col md="3" v-for="tile in experiences.tiles" :key="tile.cords.i">
+      <b-col md="6">
+        <h1 class="title">Experience</h1>
+        <p class="text-white">{{ experiences.desc }}</p>
+      </b-col>
+      <b-col md="6">
+        <b-container>
+          <b-row>
             <metro-tile
+              v-for="tile in experiences.tiles"
+              :key="tile.cords.i"
+              class="col-6 top p-0"
               :frontStyle="tile.frontStyle"
               :backStyle="{ 'background-color': 'red' }"
               :rotateX="tile.rotateX"
               @mouseover.native="tileOver(tile)"
               @mouseleave.native="resetTile(tile)"
-              :key="tile.id"
             >
-              <div slot="front">
+              <div slot="front" class="slot">
                 front
               </div>
-              <div slot="back">
+              <div slot="back" class="slot">
                 back
               </div>
-              <div slot="top">
+              <div slot="top" class="slot">
                 top
               </div>
-              <div slot="bottom">
+              <div slot="bottom" class="slot">
                 bottom
               </div>
             </metro-tile>
-          </b-col>
-        </b-row>
+          </b-row>
+        </b-container>
       </b-col>
     </b-row>
   </b-container>
 </template>
-
+<style>
+.top div {
+  width: 100% !important;
+}
+</style>
 <script>
 const Experiences = () => import("../../Experiences");
 const MetroTile = () => import("vue-metro-tile");
@@ -43,13 +52,10 @@ export default {
       experiences: localStorage.LightenedExperiences
         ? JSON.parse(localStorage.LightenedExperiences)
         : {},
-      window: window,
-      width: window.innerWidth,
-      height: window.innerHeight,
     };
   },
   mounted() {
-    Experiences().then(data => {
+    Experiences().then((data) => {
       data = data.default;
       if (
         this.experiences.version &&
@@ -75,12 +81,6 @@ export default {
     resetTile(tile) {
       tile.rotateX = 0;
     },
-  },
-  created() {
-    window.addEventListener("resize", () => {
-      this.width = window.innerWidth;
-      this.height = window.innerHeight;
-    });
   },
 };
 </script>
